dabb8e8f09c61b6a3d9ca3b676c09574a21d789c
Linda Lacsivy <llacsivy@2-B-7.42heilbronn.de>
2024-04-30 19:33:46 +0200

    prototype added in minitalk.h file

--- minitalk.h
+++ minitalk.h
@@ -6,7 +6,7 @@
 /*   By: llacsivy <llacsivy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 14:08:32 by llacsivy          #+#    #+#             */
-/*   Updated: 2024/04/30 16:19:37 by llacsivy         ###   ########.fr       */
+/*   Updated: 2024/04/30 19:33:05 by llacsivy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,6 @@
 # include <unistd.h>
 
 void	encrypt_char(pid_t pid, char c);
+void	handler(int signal);
 
 #endif
